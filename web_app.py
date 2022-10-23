74310d88d720d00e764e1d8873fac566af073da5
Rezaalvin <55049144+Rezaalvin@users.noreply.github.com>
2022-10-24 05:46:18 +0700

    Add files via upload

---
+++ web_app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, make_response
+from functools import wraps, update_wrapper
+from PIL import Image
+from predictor import Predictor
+
+import numpy as np
+import torch
+
+predictor = Predictor()
+
+# Initialize the flask application
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def upload():
+	if request.method == 'POST':
+		prediction = predictor.predict(request)
+		return prediction
+	else:
+		return render_template('home.html', prediction=None)
+
+if __name__ == '__main__':
+   app.run()
